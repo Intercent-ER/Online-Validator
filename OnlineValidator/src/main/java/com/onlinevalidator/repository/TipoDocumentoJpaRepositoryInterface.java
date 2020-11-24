a583281052517db839be802f99e734aabcd38060
GianlucaFerruzzi <gianluca.ferruzzi@scaiconsulting.com>
2020-11-24 14:20:21 +0100

    Inserito model catalog e relativo repository

--- OnlineValidator/src/main/java/com/onlinevalidator/repository/TipoDocumentoJpaRepositoryInterface.java
+++ OnlineValidator/src/main/java/com/onlinevalidator/repository/TipoDocumentoJpaRepositoryInterface.java
@@ -1,9 +1,11 @@
 package com.onlinevalidator.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.onlinevalidator.model.Tipodocumento;
 
+@Repository
 public interface TipoDocumentoJpaRepositoryInterface extends JpaRepository<Tipodocumento, Integer>{
 
 }
--- OnlineValidator/src/main/java/com/onlinevalidator/repository/ValidatorJpaRepositoryInterface.java
+++ OnlineValidator/src/main/java/com/onlinevalidator/repository/ValidatorJpaRepositoryInterface.java
@@ -3,7 +3,6 @@ package com.onlinevalidator.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.onlinevalidator.model.Tipodocumento;
 import com.onlinevalidator.model.Validatore;
 
 @Repository
