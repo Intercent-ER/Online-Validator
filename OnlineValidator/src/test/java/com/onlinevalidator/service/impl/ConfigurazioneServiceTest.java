131aa8eeb3a881335386a0732040b380ae4636c8
Manuel Gozzi <manuel.gozzi@eng.it>
2021-01-15 13:29:39 +0100

    Aggiunte classi di JUnit da completare;

---
+++ OnlineValidator/src/test/java/com/onlinevalidator/service/impl/ConfigurazioneServiceTest.java
@@ -0,0 +1,14 @@
+package com.onlinevalidator.service.impl;
+
+import org.junit.Test;
+
+public class ConfigurazioneServiceTest {
+
+	@Test
+	public void readValue() {
+	}
+
+	@Test
+	public void testReadValue() {
+	}
+}
---
+++ OnlineValidator/src/test/java/com/onlinevalidator/service/impl/ValidatorServiceTest.java
@@ -0,0 +1,27 @@
+package com.onlinevalidator.service.impl;
+
+import org.junit.Test;
+
+// TODO
+public class ValidatorServiceTest {
+
+	@Test
+	public void filtraTuttiITipiDocumento() {
+	}
+
+	@Test
+	public void getOvTipoDocumentoById() {
+	}
+
+	@Test
+	public void filtraValidatore() {
+	}
+
+	@Test
+	public void effettuaValidazione() {
+	}
+
+	@Test
+	public void validazioneSemantica() {
+	}
+}
